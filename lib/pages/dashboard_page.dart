3530ab8be63f00da9c9d299fe7510bfd3978a685
Rychner <milanrychner@gmail.com>
2025-01-23 16:56:47 +0700

    update dashboard 3

--- lib/pages/dashboard_page.dart
+++ lib/pages/dashboard_page.dart
@@ -25,30 +25,32 @@ class _DashboardPage extends State<DashboardPage> {
         children: AppConstans.navMenu.map((e) => e['view'] as Widget).toList(),
       ),
       extendBody: true,
-      bottomNavigationBar: Padding(
-        padding: const EdgeInsets.fromLTRB(50, 0, 50, 0),
-        child: Container(
-          decoration: BoxDecoration(
-            color: Colors.white,                     
-            border: Border.all(color: Colors.black12),
-            borderRadius: BorderRadius.circular(10)
-          ),
-          child: BottomNavigationBar(
-            currentIndex: _selectedIndex,
-            elevation: 0,
-            backgroundColor: Colors.transparent,
-            iconSize: 26,
-            type: BottomNavigationBarType.fixed,
-            onTap: _onItemTap,
-            showSelectedLabels: false,
-            showUnselectedLabels: false,
-            unselectedItemColor: Colors.grey[400],
-            items: AppConstans.navMenu.map((e) {
-              return BottomNavigationBarItem(
-                icon: Icon(e['icon']),
-                label: e['label'],
-              );
-            }).toList(),
+      bottomNavigationBar: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.fromLTRB(50, 0, 50, 10),
+          child: Container(
+            decoration: BoxDecoration(
+              color: Colors.white,                     
+              border: Border.all(color: Colors.black12),
+              borderRadius: BorderRadius.circular(10)
+            ),
+            child: BottomNavigationBar(
+              currentIndex: _selectedIndex,
+              elevation: 0,
+              backgroundColor: Colors.transparent,
+              iconSize: 26,
+              type: BottomNavigationBarType.fixed,
+              onTap: _onItemTap,
+              showSelectedLabels: false,
+              showUnselectedLabels: false,
+              unselectedItemColor: Colors.grey[400],
+              items: AppConstans.navMenu.map((e) {
+                return BottomNavigationBarItem(
+                  icon: Icon(e['icon']),
+                  label: e['label'],
+                );
+              }).toList(),
+            ),
           ),
         ),
       ),
